5a3176e496f3ac03bf09152510e149d36dd9451c
Joe Fatani <jucepig@gmail.com>
2021-04-28 15:26:06 -0600

    added express.static

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const messagesCtrl = require('./controllers/messages_controller')
 const app = express()
 
 app.use(express.json())
+app.use(express.static(__dirname + '/../public/build'))
 
 const PORT = 3001
 
